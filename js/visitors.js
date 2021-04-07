36f9a774c24fea732ea37769971c7e74d6f467fc
Rafin Akther Utshaw <r.akther@asthait.com>
2021-04-08 02:01:05 +0600

    visitor

--- js/visitors.js
+++ js/visitors.js
@@ -1,17 +1,16 @@
-var fs = require("fs");
-console.log("Going to write into existing file");
-// Open a new file with name input.txt and write Simply Easy Learning! to it.
-fs.writeFile('input.txt', 'Simply Easy Learning!', function(err) {
-   if (err) {
-      return console.error(err);
-   }
-   console.log("Data written successfully!");
-   console.log("Let's read newly written data");
-   // Read the newly written file and print all of its content on the console
-   fs.readFile('input.txt', function (err, data) {
-      if (err) {
-         return console.error(err);
-      }
-      console.log("Asynchronous read: " + data.toString());
-   });
-});
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log('yo')
+    getText();
+  });
+function getText(){
+    // read text from URL location
+    fetch('visitorCount.txt',{
+        headers : { 
+            'Content-Type': 'application/text',
+            'Accept': 'application/text'
+           }
+    }).then((response)=>{
+        console.log(response)
+    })
+}
